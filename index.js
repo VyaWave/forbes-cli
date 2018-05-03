a8bb43d4d516224daf467b8fcfb44e47bbd4de79
Wave <372969383@qq.com>
2018-05-03 15:23:42 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// babel解析
+require( "babel-register" )
+require( "babel-core" )
+    .transform( "code", {
+        presets: [ [ require( 'babel-preset-latest-node' ), {
+            target: 'current'
+        } ] ]
+    } );
+require( 'babel-polyfill' )
+
+require('./src')
